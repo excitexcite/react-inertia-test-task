f18e013e592bb4c7cefb99bf7ade5cc11efcea43
excitexcite <excitenew@gmail.com>
2023-05-17 07:22:35 +0300

    Initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import BaseLayout from "@/Layouts/BaseLayout";
+
+export default function Home() {
+    return (
+        <BaseLayout>
+            <h1>Home page</h1>
+        </BaseLayout>
+    );
+}
